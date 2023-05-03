e35e40f83e5f033709c022309e749d037ee36002
legol <legolas2002@mail.ru>
2023-05-03 17:30:49 +0300

    Post request for saveButton was added

--- frontend/src/components/buttons/SaveButton/SaveButton.jsx
+++ frontend/src/components/buttons/SaveButton/SaveButton.jsx
@@ -1,10 +1,17 @@
 import { useCallback, useContext } from 'react';
 import { MainContext } from '../../context/MainContext';
-
+import axios from '../../../api/axios'
 
 export default function SaveButton() {
-  const { reactFlowInstance, currentProject, setProjects, projects } = useContext(MainContext);
+  const { reactFlowInstance,
+    currentProject,
+    setProjects,
+    projects,
+    authData} = useContext(MainContext);
   const flowKey = 'load-project';
+
+  // Just for test
+  const PROJECTS_URL = '/projects'
 
   const onSave = useCallback(() => {
     if (reactFlowInstance) {
@@ -12,7 +19,7 @@ export default function SaveButton() {
       }
   }, [reactFlowInstance, currentProject])
 
-  const handleClick = () => {
+  const handleClick = async () => {
     const flow = reactFlowInstance.toObject();
     console.log(flow)
     onSave(flow)
@@ -30,7 +37,24 @@ export default function SaveButton() {
       console.log('PROJECTS: ', projects)
       
       // place for axios request
-
+      try {
+        const response = await axios.post(PROJECTS_URL, 
+            JSON.stringify({
+                name: authData.email,
+                project: {
+                  name: currentProject,
+                  instance: flow,
+                } 
+            }),
+            {
+                headers: {'Content-Type': 'application/json'},
+                withCredentials: true,
+            })
+            console.log(response.data)
+            console.log(response.accessToken)
+            console.log(JSON.stringify(response))
+    } catch(err) {
+        console.log("ERROR: ", err) }
     } else {
       console.log("TITLE_ERROR: enter title of your project")
     }
