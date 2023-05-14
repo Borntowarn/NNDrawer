eba27fedc9aaca2ab3744a62f6b333391b251627
Mikhail Kozlov <33799845+Borntowarn@users.noreply.github.com>
2023-05-14 19:23:28 +0300

    Add recursive processing nodes

--- model.py
+++ model.py
@@ -9,21 +9,35 @@ class Model(nn.Module):
 	def __init__(self) -> None:
 		super(Model, self).__init__()
 		
-		
 		self.layer_1 = nn.Sequential(
-			nn.Conv1d(in_channels=4, out_channels=8, kernel_size=3),
-			nn.Sigmoid()
+			nn.Linear(in_features=64, out_features=64),
+			nn.Linear(in_features=64, out_features=64)
 		)
     		
 		self.layer_2 = nn.Sequential(
-			nn.RNN()
+			nn.Linear(in_features=64, out_features=64),
+			nn.Linear(in_features=64, out_features=64)
+		)
+    		
+		self.layer_3 = nn.Sequential(
+			nn.ReLU(inplace=True),
+			nn.Dropout()
+		)
+    		
+		self.layer_4 = nn.Sequential(
+			nn.Tanh()
 		)
     
 
-
 	def forward(self, data):
-		data = F.batch_norm(data, )
+		# Custom node "Linear"
 		data = self.layer_1(data)
-		data = F.adaptive_avg_pool1d(data, )
+		
+		# Custom node "ReInRec"
+		data = F.avg_pool2d(data, )
 		data = self.layer_2(data)
+		data = self.layer_3(data)
+		data = F.ctc_loss(data, )
+		data = self.layer_4(data)
+		
 		return data
