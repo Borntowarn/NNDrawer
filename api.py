bdac001dcecc48b674ed590177b8806e4ca29554
Mikhail Kozlov <33799845+Borntowarn@users.noreply.github.com>
2023-05-16 11:17:43 +0300

    Export func of creating code to utils

--- api.py
+++ api.py
@@ -3,9 +3,9 @@ import uvicorn
 from typing import *
 from jose import JWTError, jwt
 from fastapi import FastAPI, Body
-from collections import defaultdict
 from datetime import datetime, timedelta
 from fastapi import FastAPI, HTTPException
+from utils.create_code import code_recursion
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends, HTTPException
@@ -116,83 +116,12 @@ async def profile(current_user: Annotated[UserOut, Depends(get_current_user)]) -
 
 
 
-def modify_objects(nodes, edges):
-    new_nodes = defaultdict(dict)
-    for node in nodes:
-        node_type = node.get('type', 'base')
-        if node_type == 'base':
-            new_nodes.update({
-                    int(node['id']): {
-                        'name': node['data'].pop('label'), 
-                        'args': node['data']['Args'] if len(node['data']['Args']) > 0 else {}
-                    }
-                }
-            )
-        elif len(node['data']['include']['nodes']) > 1:
-            new_nodes.update({
-                    int(node['id']): {
-                        'name': 'custom',
-                        'label': f'# Custom node "{node["data"].pop("label")}"',
-                        'nodes': node['data']['include']['nodes'],
-                        'edges': node['data']['include']['edges']
-                    }
-                }
-            )
-    
-    new_edges = defaultdict(dict)
-    for edge in edges:
-        new_edges.update({
-                int(edge['source']): {
-                    'id': edge['id'], 
-                    'target': int(edge['target'])
-                }
-            }
-        )
-    return new_nodes, new_edges
-
-
-def code_recursion(nodes, edges, layers, sequence, rec: int = 0, custom: str = ''):
-    if rec == 1:
-        sequence.append(f'{custom}')
-    nodes, edges = modify_objects(nodes, edges)
-    layer = """
-		self.layer_{} = nn.Sequential(
-			{}
-		)
-    """
-    curr_node = 0
-    modules = []
-    while len(edges) > 0:
-        curr_node = edges.pop(curr_node)['target']
-        if nodes[curr_node]['name'] != 'custom':
-            args = ', '.join(f'{key}={value}' for key, value in nodes[curr_node]['args'].items() if not isinstance(value, dict))
-            if not nodes[curr_node]['name'].islower():
-                modules.append(f"nn.{nodes[curr_node]['name']}({args})")
-            else:
-                if len(modules) > 0:
-                    layers.append(layer.format(len(layers) + 1, ',\n\t\t\t'.join(modules)))
-                    sequence.append(f"data = self.layer_{len(layers)}(data)")
-                    modules = []
-                sequence.append(f"data = F.{nodes[curr_node]['name']}(data{(', ' if len(args) > 0 else '') + args})")
-        else:
-            if len(modules) > 0:
-                layers.append(layer.format(len(layers) + 1, ',\n\t\t\t'.join(modules)))
-                sequence.append(f"data = self.layer_{len(layers)}(data)")
-                modules = []
-            code_recursion(nodes[curr_node]['nodes'], nodes[curr_node]['edges'], layers, sequence, rec + 1, nodes[curr_node]['label'])
-    
-    if len(modules) > 0:
-        layers.append(layer.format(len(layers) + 1, ',\n\t\t\t'.join(modules)))
-        sequence.append(f"data = self.layer_{len(layers)}(data)")
-    if rec == 1:
-        sequence.append('')
-
-
 # Обработчик для создания кода из блоков
 @app.post("/api/create_code")
 async def create_code(data: dict = Body(...)):
     nodes = data['instance']['nodes']
     edges = data['instance']['edges']
+    
     layers = []
     sequence = []
     code_recursion(nodes, edges, layers, sequence)
