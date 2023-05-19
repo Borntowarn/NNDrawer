c8bdd6a5716cc12fa2fb6960b501fe25d50860bf
legol <legolas2002@mail.ru>
2023-05-19 15:59:24 +0300

    Scroll for projects was fixed

--- frontend/src/components/areas/Dropdown/DropDown.jsx
+++ frontend/src/components/areas/Dropdown/DropDown.jsx
@@ -8,7 +8,7 @@ export default function DropDown({mode}) {
   const todoWrapper = useRef(null)
   const { projects } = useContext(MainContext)
   const [allowedProjects, setProjects] = useState(projects)
-  const hasScroll = projects.length > 6
+  const [hasScroll, setScroll] = useState(allowedProjects.length > 6)
 
 
   const handleChange = (value) => {
@@ -19,13 +19,17 @@ export default function DropDown({mode}) {
     setProjects(projects)
   }, [projects])
 
+  useEffect(() => {
+    setScroll(allowedProjects.length > 6)
+  }, [allowedProjects])
+
   useScrollbar(todoWrapper, hasScroll);
 
   return (
     <div className={mode ? 'dropdown active' : 'dropdown'}>
       <input placeholder='Project title' className='project-seacrch' onChange={(e) => handleChange(e.target.value)} type="text" />
-      <div ref={todoWrapper} className='project-list-holder'>
-        <div className={hasScroll ? 'project-list down' : 'project-list'}>
+      <div ref={todoWrapper} className={hasScroll ? 'project-list-holder down' : 'project-list-holder'}>
+        <div className='project-list'>
           {allowedProjects.map((project, i) => (
             <DropNode title={project.name} data={project} key={i}/>
           ))}
